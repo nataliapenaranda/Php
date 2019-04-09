1b6663a85a6f20a595c48189462156359626fa14
nataliapenaranda <cpenara79809@universidadean.edu.co>
2019-04-09 08:30:44 -0500

    modificacion

--- php/index.php
+++ php/index.php
@@ -2,9 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>test</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php echo 'Hola mundo'; ?>
+    <h1><?php echo 'Pagina en php'; ?></h1>
+    <h2>Hola cliente</h2>
+
+    <br>
+
+    <form action="respuesta.php" method="get">
+        <input type="text" name="usser" id="usser" placeholder="Usuario">
+        <input type="password" name="pass" id="pass" placeholder="Contraseña">
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
---
+++ php/respuesta.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Respuesta</title>
+</head>
+<body>
+    <?php
+
+    echo $_GET['usser'];
+    echo '<br>';
+    echo $_GET['pass'];
+
+    ?>
+</body>
+</html>
