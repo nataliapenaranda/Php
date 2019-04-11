56479af4069450f61bef5be6beecdf77f3a94c87
nataliapenaranda <cpenara79809@universidadean.edu.co>
2019-04-11 11:19:49 -0500

    Nuevas variables para ordenar agregadas

--- php/respuesta.php
+++ php/respuesta.php
@@ -16,11 +16,19 @@
 
     if (isset($people[$_GET['usser']])){
         if ($people[$_GET['usser']] == $_GET['pass']){
-            echo 'Bienvenido usuario' ;
+            echo '<h1>Bienvenido usuario</h1>' ;
 
 
             echo 'Lista de usuarios:';
             print_r ($people);
+
+            var estandar= sort($people);
+            echo 'Lista ordenada ascendetemente:';
+            print_r (estandar);
+
+            var invertida= asort($people);
+            echo 'Lista ordenada descendetemente:';
+            print_r (invertida);
 
         }else{
             echo 'Contraseña incorrecta';
