bcc3d52e6ff11eac7ba9f29e5ccabd2878e09d2e
nataliapenaranda <cpenara79809@universidadean.edu.co>
2019-04-11 12:12:57 -0500

    excepcion añadida

--- php/respuesta.php
+++ php/respuesta.php
@@ -16,13 +16,38 @@
     if (isset($people[$_GET['usser']])){
         if ($people[$_GET['usser']] == $_GET['pass']){
 
+             /**
+             * Devuelve el array de usuarios predeterminado
+             * @access public
+             * @param array $people users array
+             * @return $people
+             */
+
             echo '<center><h1>Bienvenido usuario</h1></center>' ;
             echo '<h2>Lista de usuarios:</h2>';
             print_r($people);
 
+            /**
+             * Devuelve el array de usuarios ordenado de manera ascendente
+             *Haciendo uso del metodo ksort, que sirve para ordenar
+             *un array de forma ascendente a partir de de sus claves
+             * @access public
+             * @param array $people users array
+             * @return $people ascendente
+             */
+
             ksort($people);
             echo '<h3>Lista ordenada ascendetemente:</h3>';
             print_r($people);
+
+            /**
+             * Devuelve el array de usuarios ordenado de manera descendente
+             *Haciendo uso del metodo krsort, que sirve para ordenar
+             *un array de forma descendente a partir de de sus claves
+             * @access public
+             * @param array $people users array
+             * @return $people descendente
+             */
 
             krsort($people);
             echo '<h4>Lista ordenada descendetemente:</h4>';
@@ -31,9 +56,14 @@
 
         }else{
             echo 'Contraseña incorrecta';
+            header("HTTP/1.0 404 Not Found");
+            header ('Location: /404.html');
+
         }
     }else{
         echo 'Usuario no encontrado';
+        header("HTTP/1.0 404 Not Found");
+        header ('Location: /404.html');
     }
 
 
