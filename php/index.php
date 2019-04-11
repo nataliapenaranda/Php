fb17d07bbee1415813b99416b3e24c87682cbfff
nataliapenaranda <cpenara79809@universidadean.edu.co>
2019-04-11 09:30:33 -0500

    corregido

--- php/index.php
+++ php/index.php
@@ -2,24 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Respuesta</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php
-    $pass = [
-    "jose" => "cordoba",
-    "manuel" => "castillo",
-    "krusty" => "ean",
-    ];
-    if (isset($pass[$_GET['usser']])){
-        if ($pass[$_GET['usser']] == $_GET['pass']){
-            echo 'Bienvenido';
-        }else{
-            echo 'Contraseña incorrecta';
-        }
-    }else{
-        echo 'Usuario no encontrado';
-    }
-    ?>
+    <h1><?php echo 'Pagina en php'; ?></h1>
+    <h2>Hola cliente</h2>
+
+    <p>Este es un ejemplo básico de PHP</p>
+
+    <br>
+
+    <form action="respuesta.php" method="get">
+        <input type="text" name="usser" id="usser" placeholder="Usuario">
+        <input type="password" name="pass" id="pass" placeholder="Contraseña">
+        <input type="submit" value="Enviar">
+    </form>
 </body>
-</html>
