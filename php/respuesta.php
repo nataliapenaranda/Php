96b44fe919ff4586c2240d7f0636fdf466435544
nataliapenaranda <cpenara79809@universidadean.edu.co>
2019-04-11 11:30:50 -0500

    agregar ;

--- php/respuesta.php
+++ php/respuesta.php
@@ -24,7 +24,7 @@
 
             $estandar = sort($people);
             echo '<h3>Lista ordenada ascendetemente:</h3>';
-            print_r ($people)
+            print_r ($people);
 
             $invertida = asort($people);
             echo '<h4>Lista ordenada descendetemente:</h4>';
